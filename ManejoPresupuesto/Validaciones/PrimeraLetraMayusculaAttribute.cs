cc2b02b5de8497528e757c0fa4165bbdae52f2f0
Alonso Anchante Moreno <alonso.amoreno@live.com>
2022-07-14 11:17:49 -0500

    Tipo cuenta CRUD

---
+++ ManejoPresupuesto/Validaciones/PrimeraLetraMayusculaAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Validaciones
+{
+    public class PrimeraLetraMayusculaAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            //return base.IsValid(value, validationContext);
+
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+            var primerLetra = value.ToString()[0].ToString();
+
+            if (primerLetra != primerLetra.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe ser mayúscula!");
+            }
+
+            return ValidationResult.Success;
+
+        }
+    }
+}
